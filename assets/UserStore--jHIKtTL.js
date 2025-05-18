a32072ac2cb31f5cbd724f949b5eeec195fa80f6
artyom <plokhikhartyom10@gmail.com>
2025-05-18 16:39:18 +0200

    Updates

--- assets/UserStore--jHIKtTL.js
+++
@@ -1,1 +0,0 @@
-import{M as e,r}from"./index-uk79Wv2n.js";const o=e("user",()=>({user:r({name:"",ic:"",dic:"",address:"",email:""})}),{persist:!0});export{o as u};
--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/invoice-maker/sw.js', { scope: '/invoice-maker/' })})}
