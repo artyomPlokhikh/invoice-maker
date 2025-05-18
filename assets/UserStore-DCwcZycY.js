e891955cfec34631106e9e6f2de9e4316e87c3d3
artyom <plokhikhartyom10@gmail.com>
2025-05-18 05:09:03 +0200

    Updates

--- assets/UserStore-DCwcZycY.js
+++
@@ -1,1 +0,0 @@
-import{L as e,r}from"./index-BCHdsTdt.js";const o=e("user",()=>({user:r({name:"",ic:"",dic:"",address:"",email:""})}),{persist:!0});export{o as u};
---
+++ assets/UserStore-DW23xVDu.js
@@ -0,0 +1,1 @@
+import{L as e,r}from"./index-DsGDX7Ci.js";const o=e("user",()=>({user:r({name:"",ic:"",dic:"",address:"",email:""})}),{persist:!0});export{o as u};
