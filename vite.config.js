4597f90296e89ae90f3a81192f79fb3b4357e76c
artyom <plokhikhartyom10@gmail.com>
2026-04-19 18:53:16 +0200

    enable Workbox cache management options

--- vite.config.js
+++ vite.config.js
@@ -39,6 +39,9 @@ export default defineConfig(({ mode }) => {
                     ]
                 },
                 workbox: {
+                    cleanupOutdatedCaches: true,
+                    skipWaiting: true,
+                    clientsClaim: true,
                     globPatterns: ['**/*.{js,css,html,ico,png,svg,mp4}'],
                     runtimeCaching: [{
                         urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
