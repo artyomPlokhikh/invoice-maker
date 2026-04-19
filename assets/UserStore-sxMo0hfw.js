c9f72bc55e7f550d2fc84bff5645326c5514e75b
artyom <plokhikhartyom10@gmail.com>
2026-04-19 18:48:39 +0200

    Updates

--- assets/UserStore-sxMo0hfw.js
+++
@@ -1,1 +0,0 @@
-import{M as e,r}from"./index-C65zzKG8.js";const o=e("user",()=>({user:r({name:"",ic:"",dic:"",address:"",email:""})}),{persist:!0});export{o as u};
---
+++ assets/UserStore-v3tys_tZ.js
@@ -0,0 +1,1 @@
+import{M as e,r}from"./index-CpRKIOAZ.js";const o=e("user",()=>({user:r({name:"",ic:"",dic:"",address:"",email:""})}),{persist:!0});export{o as u};
